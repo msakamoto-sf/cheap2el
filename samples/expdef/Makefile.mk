f3961061e1a3207fd31c02c4761440d1d3434f4a
sakamoto.gsyc.3s@gmail.com <sakamoto.gsyc.3s@gmail.com@bde153a7-42b5-2aad-85f1-2d91d7381597>
2010-07-01 10:22:07 +0000

    - modify readme.txt. - add example files for samples/expdef project. - add "/nologo" option to "lib" command.

--- samples/expdef/Makefile.mk
+++ samples/expdef/Makefile.mk
@@ -13,5 +13,13 @@ OBJS=$(PROJNAME).obj
 $(TARGET):$(OBJS)
 	$(CC) $(CFLAGS) /Fe$(TARGET) $(OBJS) $(CHEAP2EL_LIB) /link $(LFLAGS)
 
+dll2.def:$(TARGET)
+	$(CC) $(CFLAGS) /LD dll1.c
+	$(CC) $(CFLAGS) dlltest.c dll1.lib
+	$(TARGET) dll1.dll > dll2.def
+
+dll2.dll:dll2.def
+	$(CC) $(CFLAGS) /LD dll2.c dll1.lib /link /DEF:dll2.def
+
 clean:
-	del *.exe *.obj
+	del *.exe *.obj *.dll *.lib *.exp *.def
